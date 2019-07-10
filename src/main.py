99516e2b052003ab0f210767a99933a602c182f1
Filipe Laíns <lains@archlinux.org>
2019-07-10 14:17:06 +0100

    server: use OpenAPI
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- src/main.py
+++ src/main.py
@@ -1,44 +1,18 @@
 #!/usr/bin/env python3
-from time import sleep
+import connexion
+from ratbag_emu_server import encoder
 
 from protocol.hidpp20 import HIDPP20Device
 
-"""
-# Endpoint 0
-report_descriptor = [
-    0x05, 0x01,                    # Usage Page (Generic Desktop)        0
-    0x09, 0x06,                    # Usage (Keyboard)                    2
-    0xa1, 0x01,                    # Collection (Application)            4
-    0x05, 0x07,                    #  Usage Page (Keyboard)              6
-    0x19, 0xe0,                    #  Usage Minimum (224)                8
-    0x29, 0xe7,                    #  Usage Maximum (231)                10
-    0x15, 0x00,                    #  Logical Minimum (0)                12
-    0x25, 0x01,                    #  Logical Maximum (1)                14
-    0x75, 0x01,                    #  Report Size (1)                    16
-    0x95, 0x08,                    #  Report Count (8)                   18
-    0x81, 0x02,                    #  Input (Data,Var,Abs)               20
-    0x81, 0x03,                    #  Input (Cnst,Var,Abs)               22
-    0x95, 0x05,                    #  Report Count (5)                   24
-    0x05, 0x08,                    #  Usage Page (LEDs)                  26
-    0x19, 0x01,                    #  Usage Minimum (1)                  28
-    0x29, 0x05,                    #  Usage Maximum (5)                  30
-    0x91, 0x02,                    #  Output (Data,Var,Abs)              32
-    0x95, 0x01,                    #  Report Count (1)                   34
-    0x75, 0x03,                    #  Report Size (3)                    36
-    0x91, 0x01,                    #  Output (Cnst,Arr,Abs)              38
-    0x95, 0x06,                    #  Report Count (6)                   40
-    0x75, 0x08,                    #  Report Size (8)                    42
-    0x15, 0x00,                    #  Logical Minimum (0)                44
-    0x26, 0xa4, 0x00,              #  Logical Maximum (164)              46
-    0x05, 0x07,                    #  Usage Page (Keyboard)              49
-    0x19, 0x00,                    #  Usage Minimum (0)                  51
-    0x2a, 0xa4, 0x00,              #  Usage Maximum (164)                53
-    0x81, 0x00,                    #  Input (Data,Arr,Abs)               56
-    0xc0,                          # End Collection                      58
-]
-"""
+server = connexion.App('ratbag-emu',
+                specification_dir='src/server_gen/ratbag_emu_server/openapi')
+server.app.json_encoder = encoder.JSONEncoder
+server.add_api('openapi.yaml',
+                arguments={'title': 'ratbag-emu'},
+                pythonic_params=True)
+server.run(port=8080)
 
-# Endpoint 2
+# G Pro - Endpoint 2
 report_descriptor = [
     0x06, 0x00, 0xff,              # Usage Page (Vendor Defined Page 1)  0
     0x09, 0x01,                    # Usage (Vendor Usage 1)              3
