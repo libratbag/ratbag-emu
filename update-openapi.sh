d78f2d6789451cefccc1f23233a0f51aa36c8d19
Filipe Laíns <lains@archlinux.org>
2019-07-10 16:16:53 +0100

    scripts: ident update-openapi.sh
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- update-openapi.sh
+++ update-openapi.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # Server
-openapi-generator-cli generate -i src/openapi/ratbag-emu.yaml --package-name ratbag_emu_server -g python-flask -o src/server_gen/
+openapi-generator-cli generate \
+    -i src/openapi/ratbag-emu.yaml \
+    --package-name ratbag_emu_server \
+    -g python-flask \
+    -o src/server_gen/
 
 # Client
-openapi-generator-cli generate -i src/openapi/ratbag-emu.yaml --package-name ratbag_emu_client -g python -o src/client_gen/
+openapi-generator-cli generate \
+    -i src/openapi/ratbag-emu.yaml \
+    --package-name ratbag_emu_client \
+    -g python \
+    -o src/client_gen/
