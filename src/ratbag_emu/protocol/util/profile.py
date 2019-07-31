e7f24b4790256572e2f48d708900c2e2edbaf859
Filipe Laíns <lains@archlinux.org>
2019-07-31 15:18:51 +0100

    protocol: profile: create from libratbag's test profiles
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- src/ratbag_emu/protocol/util/profile.py
+++ src/ratbag_emu/protocol/util/profile.py
@@ -9,7 +9,7 @@ class Profile(object):
     buttons = []
 
     active_dpi = 0
-    dpi = [
+    x_dpi = y_dpi = [
         3000,
         6000
     ]
@@ -21,3 +21,28 @@ class Profile(object):
         [0xff, 0xff, 0xff],
         [0xff, 0xff, 0xff]
     ]
+
+    def __init__(self, obj=None):
+        if obj is None:
+            return
+        for i, profile in enumerate(obj):
+            if hasattr(profile, 'is_active'):
+                if not getattr(profile, 'is_active'):
+                    continue
+
+            if hasattr(profile, 'resolutions'):
+                for i, res in enumerate(getattr(profile, 'resolutions')):
+                    self.x_dpi[i] = getattr(res, 'xres')
+                    self.y_dpi[i] = getattr(res, 'yres')
+
+                    if hasattr(res, 'is_active'):
+                        if getattr(res, 'is_active'):
+                            self.active_dpi = i
+
+    @property
+    def dpi(self):
+        return self.x_dpi
+
+    @dpi.setter
+    def dpi(self, dpi):
+        self.x_dpi = self.y_dpi = dpi
