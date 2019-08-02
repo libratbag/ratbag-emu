96776e76c384525c013f9d6a65c7c78645f4260d
Filipe Laíns <lains@archlinux.org>
2019-08-02 14:41:18 +0100

    tests: use fixtures & add test_add_device
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,36 +1,45 @@
-from . import ratbag_emu
+import json
 
+import connexion
+import pytest
 
-def create_server():
+server = connexion.FlaskApp(__name__,
+                            specification_dir='../src/ratbag_emu/openapi/',
+                            debug=True)
+server.add_api('ratbag-emu.yaml',
+                options={"swagger_ui": True},
+                arguments={'title': 'ratbag-emu'},
+                strict_validation=True,
+                validate_responses=True)
+
+@pytest.fixture
+def handle_devices():
     import threading
 
-    import connexion
+    from . import ratbag_emu
 
     import ratbag_emu.server
     from ratbag_emu.device_handler import DeviceHandler
 
-    # Start handling devices
     devices_thread = threading.Thread(target=DeviceHandler.handle)
+    devices_thread.daemon = True
     devices_thread.start()
 
-    # Run server
-    server = connexion.FlaskApp(__name__,
-                                specification_dir='../src/ratbag_emu/openapi/',
-                                debug=True)
-    server.add_api('ratbag-emu.yaml',
-                   options={"swagger_ui": True},
-                   arguments={'title': 'ratbag-emu'},
-                   strict_validation=True,
-                   validate_responses=True)
-    server_thread = threading.Thread(target=server.run, kwargs={'port': 8080})
-    server_thread.start()
+    yield devices_thread
 
-    return server_thread
+@pytest.fixture(autouse=True)
+def client():
+    yield server.app.test_client()
 
-def test_create_server():
-    from time import sleep
+def test_list_devices(client):
+    response = client.get('/devices')
+    assert response.status_code == 200
 
-    server = create_server()
-
-    sleep(1)
-    server.shutdown()
+def test_add_device(client):
+    data = {
+        'shortname': 'steelseries-rival310'
+    }
+    response = client.post('/devices/add',
+                           data=json.dumps(data),
+                           content_type='application/json')
+    assert response.status_code == 201
