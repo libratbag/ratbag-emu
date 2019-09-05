3b8eaea64fe9ac98c3d78cfb4869fa61e0d144ee
Filipe Laíns <lains@archlinux.org>
2019-09-05 14:10:46 +0100

    profile: use dict.get() instead of try...except
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- src/ratbag_emu/protocol/util/profile.py
+++ src/ratbag_emu/protocol/util/profile.py
@@ -20,67 +20,52 @@ class Profile(object):
         '''
         Hardware properties
         '''
+
+        # DPI
         self.default_dpi = None
         self.active_dpi = None
         self.dpi = []
-        if 'resolutions' in obj:
-            for i, dpi in enumerate(obj['resolutions']):
-                self.dpi.append(dpi['xres'])
-                try:
-                    if dpi['is_active']:
-                        self.active_dpi = i
-                except KeyError:
-                    pass
-                try:
-                    if dpi['is_default']:
-                        self.default_dpi = i
-                except KeyError:
-                    pass
-            try:
-                self.dpi_step = obj['dpi_step']
-            except KeyError:
-                pass
 
+        resolutions = obj.get('resolutions', {})
+        for i, dpi in enumerate(resolutions):
+            self.dpi.append(dpi['xres'])
+            if dpi.get('is_active') == True:
+                self.active_dpi = i
+            if dpi.get('is_default') == True:
+                self.default_dpi = i
+        if obj.get('dpi_step'):
+            self.dpi_step = obj.get('dpi_step')
+
+        # Report Rates
         self.active_report_rate = 0
-        self.report_rates = [] # Hz
-        try:
-            self.report_rates = obj['report_rates']
-        except KeyError:
-            pass
-
-        try:
-            if not self.report_rates:
-                self.report_rates.append(obj['rate'])
-            else:
-                self.active_report_rate = self.report_rates.index(obj['rate'])
-        except KeyError:
-            pass
+        self.report_rates = obj.get('report_rates', []) # Hz
 
         if not self.report_rates:
-            self.report_rates.append(10000)
-            self.active_report_rate = 0
+            self.report_rates.append(obj.get('rate', 1000))
+        else:
+            self.active_report_rate = self.report_rates.index(obj['rate'])
 
+        # LEDs
         self.leds = []
-        if 'leds' in obj:
-            for led in obj['leds']:
-                try:
-                    self.leds.append(self.Led(*led['color']))
-                except KeyError:
-                    self.leds.append(self.Led(0xFF, 0xFF, 0xFF))
 
+        leds = obj.get('leds', [])
+        for led in leds:
+            if led.get('color'):
+                self.leds.append(self.Led(*led['color']))
+            else:
+                self.leds.append(self.Led(0xFF, 0xFF, 0xFF))
+
+        # Buttons
         self.buttons = []
-        if 'buttons' in obj:
-            for btn in obj['buttons']:
-                if btn['action_type'] != 'none':
-                    self.buttons.append(self.Button(btn['action_type'], btn[btn['action_type']]))
-                else:
-                    self.buttons.append(self.Button(btn['action_type']))
 
-        self.is_disabled = False
-        try:
-            self.is_disabled = obj['is_disabled']
-        except KeyError:
-            pass
+        buttons = obj.get('buttons', [])
+        for btn in buttons:
+            if btn['action_type'] != 'none':
+                self.buttons.append(self.Button(btn['action_type'], btn[btn['action_type']]))
+            else:
+                self.buttons.append(self.Button(btn['action_type']))
+
+        self.is_disabled = obj.get('is_disabled', False)
 
     def get_dpi_value(self):
         return self.dpi[self.active_dpi]
