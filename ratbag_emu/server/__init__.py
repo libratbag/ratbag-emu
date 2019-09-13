961853433f62bc1730eee97365c9795bfa211be6
Peter Hutterer <peter.hutterer@who-t.net>
2019-09-13 14:05:22 +1000

    server: compress a statement
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- ratbag_emu/server/__init__.py
+++ ratbag_emu/server/__init__.py
@@ -1,14 +1,6 @@
 # SPDX-License-Identifier: MIT
 
-from .device_controller import list_devices
-from .device_controller import add_device
-from .device_controller import get_device
-from .device_controller import delete_device
-from .device_controller import device_event
-from .device_controller import get_dpi
-from .device_controller import set_dpi
-from .device_controller import get_led
-from .device_controller import set_led
+from .device_controller import list_devices, add_device, get_device, delete_device, device_event, get_dpi, set_dpi, get_led, set_led
 
 __all__ = ['list_devices', 'add_device', 'get_device', 'delete_device',
            'device_event', 'get_dpi', 'set_dpi', 'get_led', 'set_led']
