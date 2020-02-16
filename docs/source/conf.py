0b413037e25af49b95f032dddb908a751d0bd9bf
Filipe Laíns <lains@archlinux.org>
2020-02-16 06:41:52 +0000

    docs: tweak theme
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+from pallets_sphinx_themes import ProjectLink
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
@@ -37,12 +39,16 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.intersphinx',
     'sphinx_autodoc_typehints',
+    'pallets_sphinx_themes',
     'recommonmark',
 ]
 
 # TODO: Use m2r to include the README.md instead of recommonmark. Currently waiting for a m2r release.
 
-intersphinx_mapping = {'hid-tools': ('https://pkgbuild.com/~ffy00/documentation/hid-tools/', None)}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    'hid-tools': ('https://pkgbuild.com/~ffy00/documentation/hid-tools/', None)
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -57,7 +63,18 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'flask'
+
+html_context = {
+    "project_links": [
+        ProjectLink("Source Code", "https://github.com/libratbag/ratbag-emu/"),
+        ProjectLink("Issue Tracker", "https://github.com/libratbag/ratbag-emu/issues/"),
+        ProjectLink("libratbag project", "https://github.com/libratbag/libratbag"),
+    ]
+}
+html_sidebars = {
+    "index": ["project.html", "localtoc.html", "searchbox.html"],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,22 +1,20 @@
 ratbag-emu - HID device emulator
 ================================
 
+.. image:: _static/ratbag.svg
+   :width: 30%
+   :align: right
+
+ratbag-emu is a HID device emulator built on top if the UHID API of the Linux
+kernel. It was developed with the intended use of emulating real devices in a
+CI scenario.
+
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
    Readme <README.md>
 
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
    API Documentation <modules>
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
